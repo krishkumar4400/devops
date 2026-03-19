979b681bfc67330fb9677bfb3b76b11f818bfb62
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-03-19 13:42:55 +0530

    Create an image of node ja app

---
+++ 02_docker/node-app/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res) => {
+    return res.json({
+        status: "Success",
+        message: "Hello from express server"
+    });
+});
+
+// const port = +process.env.PORT ?? 8000;
+const port = process.env.PORT ?? 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+/**
+ * In order to run this basic application we need node.js of any version (what are the requirements)
+ * 
+ * Node - v22.18.0    
+ * npm - 10.9.3
+ * may be
+ * mongoose
+ * redis
+ * etc
+ */
