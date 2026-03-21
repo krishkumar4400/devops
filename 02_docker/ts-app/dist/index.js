f3db1c724ef93be753735338eb070ac454741c89
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-03-21 16:18:11 +0530

    Build and Optimized Multi-Stage Docker Images for Production Use

--- 02_docker/ts-app/dist/index.js
+++ 02_docker/ts-app/dist/index.js
@@ -1,17 +1,13 @@
 import express from "express";
-
 const app = express();
-
 // middleware
 app.use(express.json());
-
 // routes
 app.get("/", (req, res) => {
     res.send("Hello from TypeScript + Docker 🚀");
 });
-
 app.get("/api", (req, res) => {
     res.json({ message: "API working" });
 });
-
 export default app;
+//# sourceMappingURL=index.js.map
