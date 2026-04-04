02c1d39714426e7345dea3148c1f3e584a0f9348
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-05 05:24:27 +0530

    initial commit

---
+++ 03_ci_cd/Github_Actions/node_app/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
